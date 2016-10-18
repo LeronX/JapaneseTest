62d37d551cf2dfe83d2c47f02e412521936c51ad
LeronX <LeronX@github.com>
2016-10-18 17:22:28 +0800

    create project

---
+++ app/src/main/java/org/leronx/base/Constants.java
@@ -0,0 +1,156 @@
+package org.leronx.base;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.ArrayMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+
+    //Hiragana and katakana 平假名和片假名
+
+    //基本平假名五十音
+    public final static ArrayMap<String,String> baseHiraganaMap = new ArrayMap() ;
+    static {
+        baseHiraganaMap.put("あ", "a");
+        baseHiraganaMap.put("い", "i");
+        baseHiraganaMap.put("う", "u");
+        baseHiraganaMap.put("え", "e");
+        baseHiraganaMap.put("お", "o");
+
+        baseHiraganaMap.put("か", "ka");
+        baseHiraganaMap.put("き", "ki");
+        baseHiraganaMap.put("く", "ku");
+        baseHiraganaMap.put("け", "ke");
+        baseHiraganaMap.put("こ", "ko");
+
+        baseHiraganaMap.put("さ", "sa");
+        baseHiraganaMap.put("し", "shi");
+        baseHiraganaMap.put("す", "su");
+        baseHiraganaMap.put("せ", "se");
+        baseHiraganaMap.put("そ", "so");
+
+        baseHiraganaMap.put("た", "ta");
+        baseHiraganaMap.put("ち", "chi");
+        baseHiraganaMap.put("つ", "tsu");
+        baseHiraganaMap.put("て", "te");
+        baseHiraganaMap.put("と", "to");
+
+        baseHiraganaMap.put("な", "na");
+        baseHiraganaMap.put("に", "ni");
+        baseHiraganaMap.put("ぬ", "nu");
+        baseHiraganaMap.put("ね", "ne");
+        baseHiraganaMap.put("の", "no");
+
+        baseHiraganaMap.put("は", "ha");
+        baseHiraganaMap.put("ひ", "hi");
+        baseHiraganaMap.put("ふ", "fu");
+        baseHiraganaMap.put("へ", "he");
+        baseHiraganaMap.put("ほ", "ho");
+
+        baseHiraganaMap.put("ま", "ma");
+        baseHiraganaMap.put("み", "mi");
+        baseHiraganaMap.put("む", "mu");
+        baseHiraganaMap.put("め", "me");
+        baseHiraganaMap.put("も", "mo");
+
+        baseHiraganaMap.put("や", "ya");
+        baseHiraganaMap.put("い", "i");
+        baseHiraganaMap.put("ゆ", "yu");
+        baseHiraganaMap.put("え", "e");
+        baseHiraganaMap.put("よ", "yo");
+
+        baseHiraganaMap.put("ら", "ra");
+        baseHiraganaMap.put("り", "ri");
+        baseHiraganaMap.put("る", "ru");
+        baseHiraganaMap.put("れ", "re");
+        baseHiraganaMap.put("ろ", "ro");
+
+        baseHiraganaMap.put("わ", "wa");
+        baseHiraganaMap.put("い", "i");
+        baseHiraganaMap.put("う", "u");
+        baseHiraganaMap.put("え", "e");
+        baseHiraganaMap.put("を", "o");
+
+        baseHiraganaMap.put("ん", "n");
+    }
+
+    //基本片假名五十音
+    public final static ArrayMap<String,String> baseKatakanaMap = new ArrayMap() ;
+    static {
+        baseKatakanaMap.put("ア", "a");
+        baseKatakanaMap.put("イ", "i");
+        baseKatakanaMap.put("ウ", "u");
+        baseKatakanaMap.put("エ", "e");
+        baseKatakanaMap.put("オ", "o");
+
+        baseKatakanaMap.put("カ", "ka");
+        baseKatakanaMap.put("キ", "ki");
+        baseKatakanaMap.put("ク", "ku");
+        baseKatakanaMap.put("ケ", "ke");
+        baseKatakanaMap.put("コ", "ko");
+
+        baseKatakanaMap.put("サ", "sa");
+        baseKatakanaMap.put("シ", "shi");
+        baseKatakanaMap.put("ス", "su");
+        baseKatakanaMap.put("セ", "se");
+        baseKatakanaMap.put("ソ", "so");
+
+        baseKatakanaMap.put("タ", "ta");
+        baseKatakanaMap.put("チ", "chi");
+        baseKatakanaMap.put("ツ", "tsu");
+        baseKatakanaMap.put("テ", "te");
+        baseKatakanaMap.put("ト", "to");
+
+        baseKatakanaMap.put("ナ", "na");
+        baseKatakanaMap.put("ニ", "ni");
+        baseKatakanaMap.put("ヌ", "nu");
+        baseKatakanaMap.put("ネ", "ne");
+        baseKatakanaMap.put("ノ", "no");
+
+        baseKatakanaMap.put("ハ", "ha");
+        baseKatakanaMap.put("ヒ", "hi");
+        baseKatakanaMap.put("フ", "fu");
+        baseKatakanaMap.put("ヘ", "he");
+        baseKatakanaMap.put("ホ", "ho");
+
+        baseKatakanaMap.put("マ", "ma");
+        baseKatakanaMap.put("ミ", "mi");
+        baseKatakanaMap.put("ム", "mu");
+        baseKatakanaMap.put("メ", "me");
+        baseKatakanaMap.put("モ", "mo");
+
+        baseKatakanaMap.put("ヤ", "ya");
+        baseKatakanaMap.put("イ", "i");
+        baseKatakanaMap.put("ユ", "yu");
+        baseKatakanaMap.put("エ", "e");
+        baseKatakanaMap.put("ヨ", "yo");
+
+        baseKatakanaMap.put("ラ", "ra");
+        baseKatakanaMap.put("リ", "ri");
+        baseKatakanaMap.put("ル", "ru");
+        baseKatakanaMap.put("レ", "re");
+        baseKatakanaMap.put("ロ", "ro");
+
+        baseKatakanaMap.put("ワ", "wa");
+        baseKatakanaMap.put("イ", "i");
+        baseKatakanaMap.put("ウ", "u");
+        baseKatakanaMap.put("エ", "e");
+        baseKatakanaMap.put("ヲ", "o");
+
+        baseKatakanaMap.put("ン", "n");
+    }
+
+    //基本平/片假名五十音
+    public final static ArrayMap<String,String> baseAllMap = new ArrayMap() ;
+    static {
+        baseAllMap.putAll(baseHiraganaMap);
+        baseAllMap.putAll(baseKatakanaMap);
+    }
+
+
+}
+
---
+++ app/src/main/java/org/leronx/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package org.leronx.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.ArrayMap;
+
+import org.leronx.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
